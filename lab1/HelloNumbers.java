d0500c7fa0e804db8b718dc9fa6d1cc5d616fc65
Rocky Fang <rockys910109@gmail.com>
2020-07-07 16:35:44 +0800

    cimpleted first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int acc = 0;
+        for (int i =0; i<10; i+=1){
+        	acc += i;
+        	System.out.print(acc + " ");
+        }
+        System.out.println("");
+
+    }
+}
