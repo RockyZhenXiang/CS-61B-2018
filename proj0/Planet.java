32aa621d933890459ec89ab3b390f134e9480036
Rocky Fang <rockys910109@gmail.com>
2020-07-07 19:36:30 +0800

    Project 0 Finished calcForceExertedBy

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,10 +1,15 @@
+import java.lang.Math;
+
 public class Planet{
+
 	public double xxPos;
 	public double yyPos;
 	public double xxVel;
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
+
+	public static double G = 6.67e-11;
 
 	public Planet(double xP, double yP, double xV, double yV, double m, String img){
 		xxPos = xP;
@@ -23,7 +28,23 @@ public class Planet{
 		yyVel = b.yyVel;
 		mass = b.mass;
 		imgFileName = b.imgFileName;
+	}
 
+	public double calcDistance(Planet p){
+
+		double xSqure = (xxPos-p.xxPos)*(xxPos-p.xxPos);
+		double ySqure = (yyPos-p.yyPos)*(yyPos-p.yyPos);
+
+
+		return Math.sqrt(xSqure+ySqure);
+
+	}
+
+	public double calcForceExertedBy(Planet p){
+
+		double distance = this.calcDistance(p);
+
+		return G*mass*p.mass / (distance*distance);
 	}
 
 }
