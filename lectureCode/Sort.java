78c5117a0b2b365d460a1f699a5e00098baae1fa
Rocky Fang <rockys910109@gmail.com>
2020-07-11 00:11:10 +0800

    Finished Lab3

---
+++ lectureCode/Sort.java
@@ -0,0 +1,27 @@
+public class Sort(
+
+	public static void selectionSort(String[] x){
+		// Find the smallest item.
+
+
+
+
+		// Swap it to the first.
+
+
+		// Do the remaining. 
+	}
+
+	// Return the smallest String 
+	private static String findSmallest(String[] x){
+		
+
+
+	}
+
+
+	public static void main(String[] args) {
+		
+	}
+
+	)
