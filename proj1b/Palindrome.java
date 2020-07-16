363d6f872401fd43a1a43ee83bc68d99c468b361
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-07-16 19:51:31 +0800

    Finished Task 4

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -8,4 +8,36 @@ public class Palindrome {
 
         return result;
     }
+
+    public boolean isPalindrome(String word) {
+        Deque<Character> wordDeque = wordToDeque(word);
+        if (wordDeque.size() < 2) {
+            return true;
+        }
+        int lastIndex = wordDeque.size() / 2;
+        for(int i = 0; i < lastIndex; i ++ ){
+            char head = wordDeque.get(i);
+            char tail = wordDeque.get(wordDeque.size() - 1 - i);
+            if (head != tail) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque<Character> wordDeque = wordToDeque(word);
+        if (wordDeque.size() < 2) {
+            return true;
+        }
+        int lastIndex = wordDeque.size() / 2;
+        for(int i = 0; i < lastIndex; i ++ ){
+            char head = wordDeque.get(i);
+            char tail = wordDeque.get(wordDeque.size() - 1 - i);
+            if (cc.equalChars(head, tail) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -5,6 +5,7 @@ public class TestPalindrome {
     // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
+    static OffByOne offbyone = new OffByOne();
 
     @Test
     public void testWordToDeque() {
@@ -14,5 +15,45 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
+    }
+
+    @Test
+    public void testOddChars() {
+        String test1 = "Rocky";
+        String test2 = "RidowodiR";
+        assertFalse(palindrome.isPalindrome(test1));
+        assertTrue(palindrome.isPalindrome(test2));
+    }
+
+    @Test
+    public void testEvenChars() {
+        String test1 = "Rock";
+        String test2 = "RidoodiR";
+        assertFalse(palindrome.isPalindrome(test1));
+        assertTrue(palindrome.isPalindrome(test2));
+    }
+
+    @Test
+    public void testSmallChars() {
+        String test1 = "";
+        String test2 = "R";
+        assertTrue(palindrome.isPalindrome(test1));
+        assertTrue(palindrome.isPalindrome(test2));
+    }
+
+    @Test
+    public void testEvenChars2() {
+        String test1 = "RidowodiR";
+        String test2 = "flake";
+        assertFalse(palindrome.isPalindrome(test1, offbyone));
+        assertTrue(palindrome.isPalindrome(test2, offbyone));
+    }
+
+    @Test
+    public void testSmallChars2() {
+        String test1 = "";
+        String test2 = "R";
+        assertTrue(palindrome.isPalindrome(test1, offbyone));
+        assertTrue(palindrome.isPalindrome(test2, offbyone));
     }
 }
