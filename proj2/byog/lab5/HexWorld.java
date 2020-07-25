c2f11d06353444c24cc59031b1c6988704225511
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-07-25 17:11:07 +0800

    Finished two protected method of addHexagon()

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -13,11 +13,28 @@ import java.util.Random;
  */
 public class HexWorld {
 
-    private static boolean isInHexagon() {
-
-        return true; 
-
+    protected static boolean isInHexagon(int column, int row, int size) {
+        if (row >= size) {
+          row = 2 * size - 1 - row;
+        }
+        return (column >= size - row - 1 && 2 * size + row - 1 > column);
     }
+
+    protected static void changeContent(int[][] world, int x, int y, int size ) {
+        int width = 3 * size - 2;
+        int height = 2 * size;
+
+        for (int yIndex = y; yIndex - y < height; yIndex ++ ) {
+            for (int xIndex = x; xIndex - x < width; xIndex ++) {
+                int column = xIndex - x;
+                int row = yIndex - y;
+                if (isInHexagon(column, row, size)) {
+                    world[yIndex][xIndex] = 1;
+                }
+            }
+        }
+    }
+
 
     /**
      * Add a Hexagon to a wold with given location, length, and tile type
@@ -25,14 +42,23 @@ public class HexWorld {
      * @param x the x coordinate of the upper-left of the hexagon
      * @param y the y coordinate of the upper-left of the hexagon
      *               x
-     *          y -> **
-     *              ****
-     *              ****
-     *               **
+     *          y ->  **
+     *               ****
+     *               ****
+     *                **
      * @param size the length of the hexagon
      * @param t the type of the tile
      */
     public static void addHexagon(TETile[][] world, int x, int y, int size, TETile t) {
+        int width = 3 * size - 2;
+        int height = 2 * size;
+
+        for (int yIndex = y; yIndex - y < height; yIndex ++ ) {
+            for (int xIndex = x; xIndex - x <width; xIndex ++) {
+
+            }
+
+        }
 
     }
 }
--- proj2/byog/lab5/TestHexWorld.java
+++ proj2/byog/lab5/TestHexWorld.java
@@ -3,5 +3,41 @@ package byog.lab5;
 import org.junit.Test;
 import org.junit.Assert.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class TestHexWorld {
+
+    @Test
+    public void testIsInHexagon() {
+        int size = 4;
+        int width = 3 * size - 2;
+        int height = 2 * size;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                boolean res = HexWorld.isInHexagon(i, j, size);
+                System.out.println("i = " + i + ", j = " + j + ", result = " + res);
+                if (i == width - 1) {
+                    System.out.println("");
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testChange() {
+        int [][] wor= new int[10][10];
+        for (int[] a: wor) {
+            System.out.println(Arrays.toString(a));
+        }
+        int X = 2;
+        int Y = 3;
+        int size = 2;
+        System.out.println("");
+
+        HexWorld.changeContent(wor, X, Y, size);
+        for (int[] a: wor) {
+            System.out.println(Arrays.toString(a));
+        }
+    }
 }
