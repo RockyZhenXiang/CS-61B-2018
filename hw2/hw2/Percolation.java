021e840ce92bd021d15d5ed658b8b70001e0c687
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-08-03 20:18:09 +0800

    Finish open()

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -13,9 +13,11 @@ public class Percolation {
      * @param N length of the grid
      */
     public Percolation(int N) {
+        if (N < 0) {
+            throw new IllegalArgumentException();
+        }
         grid = new boolean[N][N];
         dj = new WeightedQuickUnionUF(N*N);
-
     }
 
     /**
@@ -24,7 +26,7 @@ public class Percolation {
      * @param col x coordinate of the tile
      * @return hash code of [row, col]
      */
-    private int xy2iD(int row, int col) {
+    private int xy2id(int row, int col) {
         int n = grid.length;
         return row * n + col;
     }
@@ -36,10 +38,42 @@ public class Percolation {
      */
     public void open(int row, int col) {
         if (!isOpen(row, col)) {
-
+            grid[row][col] = true;
+            if (openedSite != 0) {
+                int [] openedNei = neighborOpen(row, col);
+                for (int id: openedNei) {
+                   if (id >= 0) {
+                       dj.union(xy2id(row, col), id);
+                   }
+                }
+            }
+            openedSite += 1;
         }
     }
 
+
+    /**
+     * returns the id of the opened site surrounding [row, col]
+     * @param row y coordinate of the tile
+     * @param col x coordinate of the tile
+     * @return the id of the opened site surrounding [row, col]
+     */
+    private int[] neighborOpen(int row, int col) {
+        int n = grid.length;
+        int[][] check = new int[][]{new int[]{row, col + 1},
+                                    new int[]{row, col - 1},
+                                    new int[]{row + 1, col},
+                                    new int[]{row - 1, col}};
+        int[] res = new int[]{-1, -1 ,-1 ,-1};
+        for (int i = 0; i < 4; i += 1) {
+            if (isInBound(check[i][0], check[i][1])) {
+                if (isOpen(check[i][0], check[i][1])) {
+                    res[i] = xy2id(check[i][0], check[i][1]);
+                }
+            }
+        }
+        return res;
+    }
 
 
     /**
@@ -49,9 +83,25 @@ public class Percolation {
      * @return true if the site is open
      */
     public boolean isOpen(int row, int col) {
+        if (!isInBound(row,col)) {
+            throw new IndexOutOfBoundsException(row + " " + col);
+        }
 
-        return false;
+        return grid[row][col];
     }
+
+    /**
+     * check if [row, col] is in the grid
+     * @param row y coordinate of the tile
+     * @param col x coordinate of the tile
+     * @return if [row, col] is in the grid
+     */
+    private boolean isInBound(int row, int col) {
+
+        return row < grid.length && row >= 0 &&
+                col < grid.length && col >= 0;
+    }
+
 
     /**
      * is the site (row, col) full?
@@ -69,8 +119,7 @@ public class Percolation {
      */
     public int numberOfOpenSites() {
 
-
-        return 0;
+        return openedSite;
     }
 
     /**
@@ -84,6 +133,9 @@ public class Percolation {
 
     public static void main(String[] args) {
         Percolation per = new Percolation(5);
+        per.open(2,3);
+        per.open(2,4);
+        per.open(5,2);
 
     }
 
