ce25d47e9137fb026c96324eaf93a504b96b224c
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-08-05 13:24:02 +0800

    Finished swim(), sink()

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -27,25 +27,24 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      * Returns the index of the left child of the node at i.
      */
     private static int leftIndex(int i) {
-        /* TODO: Your code here! */
-
-        return 0;
+        return 2 * i;
     }
 
     /**
      * Returns the index of the right child of the node at i.
      */
     private static int rightIndex(int i) {
-        /* TODO: Your code here! */
-        return 0;
+        return 2 * i + 1;
     }
 
     /**
      * Returns the index of the node that is the parent of the node at i.
      */
     private static int parentIndex(int i) {
-        /* TODO: Your code here! */
-        return 0;
+        if (i == 1) {
+            return 1;
+        }
+        return i / 2;
     }
 
     /**
@@ -107,9 +106,11 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     private void swim(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
-
-        /** TODO: Your code here. */
-        return;
+        int parentId = parentIndex(index);
+        if (min(parentId, index) == index && index != 1) {
+            swap(parentId, index);
+            swim(parentId);
+        }
     }
 
     /**
@@ -119,8 +120,19 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
-        /** TODO: Your code here. */
-        return;
+        int leftId = leftIndex(index);
+        int rightId = rightIndex(index);
+        int smallerChild = min(leftId, rightId);
+
+        if (getNode(smallerChild) == null) {
+            return;
+        }
+
+        if (min(smallerChild, index) == smallerChild)  {
+            swap(smallerChild, index);
+            sink(smallerChild);
+        }
+
     }
 
     /**
