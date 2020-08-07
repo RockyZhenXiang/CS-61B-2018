dcba3459ea3579ff406c7a0a3f05bd0229aaec8a
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-08-07 17:19:27 +0800

    Finished breadfirst

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,11 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
- *  @author Josh Hug
+ *  @author Rocky Zhenxiang Fang
+ *  @soruse Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -10,20 +14,52 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s; // source id
+    private int t; // target id
+    private boolean targetFound = false;
+    private Maze maze;
+    private Queue<Integer> fringe;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        fringe = new ArrayDeque();
+        fringe.add(maze.xyTo1D(sourceX, sourceY));
+
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        while (!fringe.isEmpty()) {
+            int current = fringe.poll();
+            marked[current] = true;
+            announce();
+
+            if (current == t) {
+                targetFound = true;
+                return;
+            }
+
+            for (int nei: maze.adj(current)) {
+                if (!marked[nei]) {
+                    fringe.add(nei);
+                    distTo[nei] = distTo[current] + 1;
+                    edgeTo[nei] = current;
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
